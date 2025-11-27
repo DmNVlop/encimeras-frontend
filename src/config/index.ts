bb0b2b7632dabb59b671e38648f93a8323a52f76
Damian Vidal <dmnvlop@gmail.com>
2025-11-27 13:00:51 +0100

    FIX: Readme

--- src/config/index.ts
+++ src/config/index.ts
@@ -2,12 +2,12 @@
 
 export const config = {
   api: {
-    baseURL: import.meta.env.VITE_API_URL || "http://localhost:30001",
+    baseURL: import.meta.env.VITE_API_URL || "http://localhost:3000",
     timeout: 10000, // Tiempo máximo de espera
   },
   assets: {
     // Aquí aplicamos la lógica del prefijo que hablamos antes
-    baseUrl: import.meta.env.VITE_ASSETS_URL || "http://localhost:30001/",
+    baseUrl: import.meta.env.VITE_ASSETS_URL || "http://localhost:3000/",
   },
   // Puedes agregar otras configuraciones globales aquí
   app: {
