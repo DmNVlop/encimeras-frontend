56bb785fc7d30c88755a14e906a65edddb75fd41
Damian Vidal <dmnvlop@gmail.com>
2026-02-11 11:40:18 +0100

    feat: ordenes de usuarios

--- src/services/orders.service.ts
+++ src/services/orders.service.ts
@@ -1,7 +1,12 @@
-import { update } from "@/services/api.service"; // Tu utilidad genérica
+import { update, get } from "@/services/api.service"; // Tu utilidad genérica
 
 export const ordersApi = {
   // Método específico y tipado
+  findAll: async (status?: string) => {
+    const params = status ? { status } : {};
+    return get<any[]>("/orders", { params }); // Ajustar tipo de retorno a Order[] cuando sea posible importarlo aqui o dejar any[] y castear
+  },
+
   updateStatus: async (orderId: string, status: "APPROVED" | "REJECTED" | "PENDING") => {
     // Aquí usamos tu utilidad genérica
     // Endpoint: '/orders'
